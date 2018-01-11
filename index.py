aed336530576f9dbee5251c2a16d861d841eee57
napalm255 <napalm255@gmail.com>
2018-01-10 20:27:19 -0500

    initial

--- index.py
+++ index.py
@@ -1,12 +1,48 @@
+#!/usr/bin/env python
+"""Commute SNS to Database."""
+# pylint: disable=broad-except
+
+from __future__ import print_function
+import sys
+import logging
 import json
-import datetime
+import boto3
+
+
+# logging configuration
+logging.getLogger().setLevel(logging.INFO)
+
+try:
+    SSM = boto3.client('ssm')
+
+    PREFIX = '/ddns/config'
+    PARAMS = SSM.get_parameters_by_path(Path=PREFIX, Recursive=True,
+                                        WithDecryption=True)
+    logging.debug('ssm: parameters (%s)', PARAMS)
+
+    CONFIG = dict()
+    for param in PARAMS['Parameters']:
+        key = param['Name'].replace('%s/' % PREFIX, '')
+        CONFIG.update({key: param['Value']})
+    logging.debug('ssm: config (%s)', CONFIG)
+
+    logging.info('ssm: successfully gathered parameters')
+except Exception as ex:
+    logging.error('ssm: could not connect to SSM. (%s)', ex)
+    sys.exit()
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
+    """Lambda handler."""
+    # pylint: disable=unused-argument, too-many-locals
+    logging.info(event)
+
+    header = {'Content-Type': 'application/json'}
+
     return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+            'body': json.dumps({'status': 'OK'}),
+            'headers': header}
+
+
+if __name__ == '__main__':
+    print(handler({'message': ''}, None))
